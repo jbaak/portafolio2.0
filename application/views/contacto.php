32ce71155134f9af3a841ef1d617b028aba9038a
Jesus Baak <jesusbaak@hotmail.com>
2012-09-13 15:33:45 -0500

    modificando contacto

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -1,6 +1,31 @@
 
 <section id="content">
-	<p><h2>Contactanos</h2></p>
+	<p><h2>Cont&aacute;ctanos</h2></p>
+
+	<article id="info-contacto">
+		Merida, Yucatan, M&eacute;xico.
+		<br/>
+		<br/>
+		Email:
+		<span class="datos-cotacto">jesusbaak@hotmail.com</span>
+		<br />
+		<br />
+		M&oacute;vil:
+		<span class="datos-cotacto">(+52)9991 983710</span>
+		<br />
+		<br />
+		Social Media:
+		<br />
+		<br />
+		<p>
+			 <a href="http://facebook.com/jesusbaak" class="fade" title="Facebook" target="_blank"><img src="<?php echo base_url(); ?>images/iconos/Face_c.png"/></a>
+			 <a href="http://twitter.com/jbaak" class="fade" title="Twitter" target="_blank"><img src="<?php echo base_url(); ?>images/iconos/Twitter_c.png"/></a>
+			 <a href="http://github.com/jbaak" class="fade" title="github" target="_blank"><img src="<?php echo base_url(); ?>images/iconos/Github_c.png"/></a>
+
+
+		</p>
+		
+	</article>
 
 	<article id="contactanos">
 			<?php echo form_open('home/envio'); ?>
@@ -9,7 +34,6 @@
 				$name = array(
 							'name' => 'name',
 							'id' => 'name',
-							'class'=>'fade',
 							'required'=>'',
 							'value' => set_value('name')
 
@@ -31,16 +55,21 @@
 							'name' => 'mensaje',
 							'id'=>'mensaje',
 							'required'=>'',
-							'rows'=>'6',
+							'rows'=>'4',
 							'cols'=>'50',
 							'id' => set_value('mensaje')
 							
 				);
+
+				$boton =array(
+							'type'=>'submit',
+							'class'=> 'fade'
+					);
 				 ?>
 
 				 <fielset>
 				 	<legend>Para mas informacion envie un correo llenando los datos del formulario </legend>
-				 </fieldset>
+				 
 				 	<div>	
 						 <?php echo form_label('Nombre: ') ?>
 						 <?php echo form_input($name); ?>
@@ -58,14 +87,19 @@
 						<?php echo form_textarea($mensaje); ?>
 					</div>
 					<div>	
-						<?php echo form_submit('submit','Enviar'); ?>
+						<?php echo form_submit($boton,'Enviar'); ?>
 					</div>	
+			
+						
+
+			<div>
+			<?php echo  validation_errors(); ?>
+			</div>
+
+			</fieldset>
+
 			<?php echo form_close()?>
 		</article>
-
-		<div>
-		<?php echo  validation_errors(); ?>
-		</div>
 	
 		
 	
--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -8,7 +8,7 @@
 		</div>
 		<div class="col_d">
 			<h2>FOLLOW</h2>
-			<!-- <p><a href="" target="_blank"><img src="images/iconos/Face_c.png"/></a><a href="" target="_blank"><img src="images/iconos/Twitter_c.png"/></a><a href="" target="_blank"><img src="images/iconos/Github_c.png"/></a></p> -->
+			
 			<p><a href="" target="_blank">f</a> <a href="" target="_blank">t</a> <a href="" target="_blank">github</a></p>
 		</div>
 
