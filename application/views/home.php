e6ff681c885f9ef371f083c2e34b7827d3aa976a
Jesus Baak <jesusbaak@hotmail.com>
2012-09-13 17:02:55 -0500

    Agregando botones sociales

--- application/views/home.php
+++ application/views/home.php
@@ -3,14 +3,26 @@
 	<p><h2>Licenciado en Ciencias Computacionales</h2></p>
 
 	
-		<div id="mapa">
-		<p>Mapa</p>
-		</div>
+		<aside>
+		<br/><br/>
+		<!--twitter-->
+		<script src="http://platform.twitter.com/widgets.js"></script>
+		<a href="http://twitter.com/share" class="twitter-share-button" data-text="Web Developer Jesus Baak" data-url="http://mitisio.com" data-count="vertical">Twittear</a>
+		<br/><br/>
+		<!-- facebook-->
+		<div id="fb-root"></div>		
+			<script src="http://connect.facebook.net/es_LA/all.js#xfbml=1"></script>
+			<fb:like href="http://facebook.com/mipaginafacebook" send="false" layout="box_count" show-faces="false" font="tahoma"></fb:like>
+		<br/><br/>
+
+		</aside>
 
 
-	<div id="acerca">
-		<p>CONOCIMIENTOS EN <span>PHP</span> ESTRUCTURADO, POO, MVC, MANEJO LOS FRAMEWORKS <span>ZANPHP</span> Y <span>CODEIGNITER</span>. LOS CMS <span>JOOMLA</span> Y <span>WORDPRESS</span>( DESARROLLO THEMES), <span>JAVASCRIPT</span> (MANEJO <span>AJAX</span> Y <span>JQUERY</span>), <span>HTML5</span> y <span>CSS3</span>. LENGUAJE DE CONSULTA ESTRUCTURADO <span>SQL</span> (MOTOR DE BASE DE DATOS <span>MYSQL</span>). MANEJO EL FRAMEWORK BOOSTRAP DE TWITTER, LA API DE FACEBOOK Y LA DE GOOGLE MAPS.</p>
-	</div>
+	<section id="acerca">
+		<article>
+		<p>Conocimientos en <span>PHP</span> Estructurado, POO, MVC, manejo los frameworks <span>ZANPHP</span> y <span>CODEIGNITER</span>. Los CMS <span>JOOMLA</span> y <span>WORDPRESS</span>, <span>JAVASCRIPT</span> (manejo <span>AJAX</span> y <span>JQUERY</span>), <span>HTML5</span> y <span>CSS3</span>. Lenguaje de consulta estructurado <span>SQL</span> ( motor de base de datos <span>MYSQL</span>). Manejo el Framework boostrap de  TWITTER, la api de FACEBOOK y la de Google Maps.</p>
+		</article>
+	</section>
 	
 </section>
 
