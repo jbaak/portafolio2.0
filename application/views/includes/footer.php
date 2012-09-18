b5a072e2520113f751f9c86e606f94cbae5e37de
Jesus Baak <jesusbaak@hotmail.com>
2012-09-17 21:45:34 -0500

    modificando footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -5,15 +5,15 @@
 			<h2>Powered with</h2>
 			<p><a href="http://www.w3.org/html/logo/">
 <img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="140" height="55" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics">
-</a></li></p>
-			<p>Codeignither</p>
+</a> <a href="" tarjet="_blank">Codeignither</a></p>
+			
 
 			
 		</div>
 		<div class="col_d">
-			<h2>FOLLOW</h2>
+			<h2>desarrollado por</h2>
 			
-			<p><a href="" target="_blank">f</a> <a href="" target="_blank">t</a> <a href="" target="_blank">github</a></p>
+			<p><a href="" target="_blank">@jbaak</a></p>
 		</div>
 
 	</div>	
