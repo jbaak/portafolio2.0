2f64af6285237cb2fafee02a9e9d41982b7e8e1a
Jesus Baak <jesusbaak@hotmail.com>
2012-09-19 14:27:29 -0500

    modificando acerca de mi

--- application/views/home.php
+++ application/views/home.php
@@ -23,7 +23,7 @@
 		<p><h2>Acerca de mi</h2></p>
 		<p>Mi nombre es Jesus Baak soy Licenciado en Ciencias Computacionales, tengo 27 años de edad y  soy originario de  Merida, Yucatan, M&eacute;xico :D</p>
 		<p>Me puedo definir como un "Autodidacta, Apasionado por el desarrollo de aplicaciones Web y las nuevas tecnologías" soy una persona que esta en constante aprendizaje. </p>
-		<p>Aguila de corazon y adicto al cafe xD </p>
+		<p>Aguila de corazon y adicto al cafe ... por cierto si estas mirando esto desde internet explorer, de verdad ... no eres digno de ver bordes redondeados ni sombritas xD </p>
 		
 		<p><h2>Conocimientos</h2></p>
 		<p><span>PHP</span> <span>Estructurado</span> <span>POO</span> <span>MVC</span> <span>ZANPHP</span> <span>CODEIGNITER</span> <span>JOOMLA</span> <span>WORDPRESS</span> <span>JAVASCRIPT</span> <span>AJAX</span> <span>JQUERY</span> <span>HTML5</span> <span>CSS3</span> <span>SQL</span> <span>MYSQL</span> <span>Framework boostrap de  TWITTER</span> <span>api de FACEBOOK</span> <span>api de Google Maps</span></p>
