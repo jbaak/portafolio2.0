e9bbf61a47deee8bd65845e0d9483626bd1cecac
Jesus Baak <jesusbaak@hotmail.com>
2012-10-03 10:48:27 -0500

    agregando html5shim

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -42,6 +42,11 @@
 		});
 	</script>
 
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
 
 	
 
