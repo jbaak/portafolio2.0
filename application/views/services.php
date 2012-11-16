f7f224a7896fd91287c194c4681bd95ea7a7d972
Jesus Baak <Jesusbaak@hotmail.com>
2012-11-16 16:53:25 -0600

    terminado el estilo de servicios

--- application/views/services.php
+++ application/views/services.php
@@ -1,8 +1,58 @@
 
 <section id="content">
-	
-		<p><h2>Servicios</h2></p>
-		
+		<section id="servicios-principal">
+			<p><h2>Servicios</h2></p>
+			<img id="img-servicios" src="<?php echo base_url();?>images/baak.jpg">
+			<article id="nuestros-servicios">
+				<h3>Nuestros servicios</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>	
+
+			</article>
+		</section>
+
+		<section id="servicios-tipos">
+			<p><h2>Tipos</h2></p>
+			<article id="servicio1">
+				<h3>Servicio1</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</article>	
+			<article id="servicio2">
+				<h3>Servicio2</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</article>
+			<article id="servicio3">
+				<h3>Servicio3</h3>
+				<ol>
+					<li>elemento 1</li>
+					<li>elemento 1</li>
+					<li>elemento 1</li>
+					<li>elemento 1</li>
+					<li>elemento 1</li>
+					
+				</ol>
+			</article>
+		</section>
 	
 </section>
 
