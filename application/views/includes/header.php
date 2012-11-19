751174f1601dda6ccdcfa9076ac88b7d08b31689
Jesus Baak <Jesusbaak@hotmail.com>
2012-11-19 11:53:27 -0600

    agregando responsive.css

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -8,6 +8,7 @@
 	
 
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css" >
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/responsive.css" >
 	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.js"></script>
 	<script type="text/javascript" src="<?php echo base_url(); ?>js/funciones.js"></script>
 
